a693832f55b3c684a1fa6d6b24ccff8193159ddc
colin <colin@LAPTOP-U0C7GGMQ>
2021-04-30 17:48:48 +0100

    Added View::factory

--- src/clinicpms/view/View.java
+++ src/clinicpms/view/View.java
@@ -5,8 +5,10 @@
  */
 package clinicpms.view;
 
+import clinicpms.controller.EntityDescriptor;
 import clinicpms.view.interfaces.IView;
 import clinicpms.view.interfaces.IViewInternalFrameListener;
+import java.awt.event.ActionListener;
 import java.beans.PropertyChangeListener;
 import java.time.format.DateTimeFormatter;
 import javax.swing.JInternalFrame;
@@ -17,9 +19,47 @@ import javax.swing.JInternalFrame;
  */
 public abstract class View extends JInternalFrame
                            implements PropertyChangeListener,IView, IViewInternalFrameListener{
+    private static Viewer viewer = null;
     
     public View(){
         super("Appointments view",true,true,true,true);
         
+    }
+    
+    public static enum Viewer { APPOINTMENT_SCHEDULE_VIEW,
+                                APPOINTMENT_CREATOR_VIEW,
+                                APPOINTMENT_CREATOR_EDITOR_VIEW,
+                                APPOINTMENT_EDITOR_VIEW,
+                                PATIENT_VIEW}
+    
+    public static Viewer getViewer(){
+        return viewer;
+    }
+    
+    public static void setViewer(Viewer value){
+        viewer = value;
+    }
+    
+    public static View factory(ActionListener controller, EntityDescriptor ed, DesktopView dtView){
+        View result = null;
+        switch(getViewer()){
+            case APPOINTMENT_SCHEDULE_VIEW:
+                result = new AppointmentsForDayView(controller, ed);
+                break;
+            case APPOINTMENT_CREATOR_VIEW:
+                result = null;
+                break;
+            case APPOINTMENT_CREATOR_EDITOR_VIEW:
+                result = new AppointmentCreatorEditorModalViewer(controller, ed, dtView.getContentPane());
+                break;
+            case APPOINTMENT_EDITOR_VIEW:
+                result = null;
+                break;
+            case PATIENT_VIEW:
+                result = new PatientView(controller, ed);
+                break;
+                
+        }
+        return result;
     }
 }
